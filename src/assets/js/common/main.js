b2d31990a6feec57953723f8042bff0ef95731e9
cameronclass <kamrankutumov@gmail.com>
2020-12-31 03:40:47 +0300

    initial commit

--- src/assets/js/common/main.js
+++ src/assets/js/common/main.js
@@ -20,4 +20,31 @@ $(document).ready(function () {
   onElementHeightChange(document.body, function () {
     AOS.refresh();
   }); */
+
+  // Show the first tab and hide the rest
+  $("#tabs-nav li:first-child").addClass("active");
+  $(".tab-content").hide();
+  $(".tab-content:first").show();
+
+  // Click function
+  $("#tabs-nav li").click(function (e) {
+    e.preventDefault();
+    $("#tabs-nav li").removeClass("active");
+    $(this).addClass("active");
+    $(".tab-content").hide();
+
+    var activeTab = $(this).find("a").attr("href");
+    $(activeTab).fadeIn();
+    return false;
+  });
+
+  $(".accordion__header").click(function () {
+    $(".accordion__body").not($(this).next()).slideUp(400);
+    $(this).next().slideToggle(400);
+
+    $(".accordion__item")
+      .not($(this).closest(".accordion__item"))
+      .removeClass("open-accordion");
+    $(this).closest(".accordion__item").toggleClass("open-accordion");
+  });
 });
